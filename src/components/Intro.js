6928e0795e690faa0a3b70d352647dceabeff795
pratyushgolwala <pratyushgolwala@gmail.com>
2025-03-14 19:34:23 +0530

    initial commit

---
+++ src/components/Intro.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import myImage from "./mypic.png"
+
+export default function Intro() {
+    return (
+        <>
+            <div className="Contaner d-flex">
+                <div className="Container1">
+                    <div className="InnerDiv d-flex">
+                        <div className='circle'></div>
+                        <p>Available For Work</p>
+                    </div>
+                    <div>
+                        <h3>SOFTWARE ENGINEER</h3>
+                      
+                    </div>
+                    
+                </div>
+                <div className="Container2">
+                    <div className="profile-container">
+                        <img src={myImage} alt="My Profile" className="profile-image" />
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg">
+                <a className="navbar-brand" href="/">Pratyush Golwala</a>
+                <div className="btn-group">
+                    <button className="btn">HOME</button>
+                    <button className="btn">ABOUT</button>
+                    <button className="btn">WORK</button>
+                </div>
+                <button style={{ fontSize: "24px", backgroundColor: "#2b2b2b", padding: "3px" }}>
+  <i className="fas fa-phone-square-alt" style={{ color: "gray" }}></i>
+</button>
+
+
+
+
+            </nav>
+        </>
+    )
+}
